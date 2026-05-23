3aa6930f3ea2238701cc10af5716bf7e97ecfefd
Odenec <paranoyyyaaa@yandex.ru>
2026-05-23 07:50:38 +0300

    android-app

---
+++ 05-university/02-02-OS/AndroidApp/AndroidApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace AndroidApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void OnCalculate(object sender, EventArgs e)
+    {
+
+        double.TryParse(FirstInput.Text, out double num1);
+
+
+        double.TryParse(SecondInput.Text, out double num2);
+
+
+        double sum = num1 + num2;
+
+
+        ResultLabel.Text = "Результат: " + sum.ToString();
+    }
+}
---
+++ 05-university/02-02-OS/AndroidApp/AndroidApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AndroidApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
